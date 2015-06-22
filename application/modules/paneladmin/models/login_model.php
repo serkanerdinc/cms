1f7ef79514f61695ccacb3ebf3a1d9ae5b2786d0
Serkan Erdinç <serkanerdinc@windowslive.com>
2015-06-22 18:04:35 +0300

    cms 0.0.3

--- application/modules/paneladmin/models/login_model.php
+++ application/modules/paneladmin/models/login_model.php
@@ -5,12 +5,13 @@ class login_model extends CI_Model {
 	public function loginkontrol($data)
 	{
 		$kk = $this->xdb->query_array("SELECT * FROM admin WHERE username='".$data["user"]."' and password='".$data["password"]."' and is_active=1");
+		echo "SELECT * FROM admin WHERE username='".$data["user"]."' and password='".$data["password"]."' and is_active=1";
 		if ($kk)
 		{
 			$session["userid"] = $kk[0]["id"];
 			$session["username"] = $kk[0]["username"];
 			$session["mail"] = $kk[0]["mail"];
-			$session["0b59c1eca2014d487be844846"] = TRUE;
+			$session["8f950a73d74a9e8fafdcebc961d7b14c"] = TRUE;
 			$this->session->set_userdata($session);
 			return $kk[0];
 		}
